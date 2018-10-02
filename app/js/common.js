90281bb67d2e18106d06ed852bb0d3bc44c912c1
Vonasur <rusanov@vonasur.ru>
2018-10-02 21:42:42 +0200

    Autoservice39

---
+++ app/js/common.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+        new WOW().init();
+    $('.bxslider').bxSlider({
+        pager: false,
+        controls: true,
+        nextText: '',
+        prevText: '',
+        responsive: true
+    });
+});
---
+++ readme.md
@@ -0,0 +1,7 @@
+<h1>Autoservice39</h1>
+
+<p>
+	Сайт компании, предоставляющей услуги автосервиса, автостоянки и продаже запчастей.
+</p>
+
+<p>Bootstrap 4, JQuery, Animate.css, Bxslider, Sass, Gulp</p>
